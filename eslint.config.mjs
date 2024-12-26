23af739ee4601b5862c5099e99b2eadef82e4d92
Ayo Ayco <ayo@ayco.io>
2024-12-26 01:08:14 +0100

    chore: set up eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,41 @@
+import globals from 'globals'
 import eslintPluginAstro from 'eslint-plugin-astro'
-import jsPlugin from '@eslint/js';
+import jsPlugin from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import astroParser from 'astro-eslint-parser'
 
 export default [
-  // add more generic rule sets here, such as:
-  jsPlugin.configs.recommended,
-  ...eslintPluginAstro.configs['recommended'],
-  ...eslintPluginAstro.configs['jsx-a11y-recommended'],
-  {
-    ignores: [
-      'dist/*',
-      '.output/*',
-      '.nitro/*',
-      'node-modules*',
-      'site/*',
-      'templates/*',
-    ],
-  },
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+	},
+	// add more generic rule sets here, such as:
+	jsPlugin.configs.recommended,
+	...tseslint.configs.recommended,
+	...eslintPluginAstro.configs['recommended'],
+	...eslintPluginAstro.configs['jsx-a11y-recommended'],
+	{
+		ignores: [
+			'dist/*',
+			'.output/*',
+			'.astro/*',
+			'site/*',
+			'templates/*',
+			'**/node_modules/*',
+			'**/env.d.ts',
+		],
+	},
+	{
+		files: ['**/*.astro'],
+		languageOptions: {
+			parser: astroParser,
+			parserOptions: {
+				parser: tseslint.parser,
+			},
+		},
+	},
 ]
